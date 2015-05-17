2d8095da61c5a7f4659f5ee528be0435f71399c4
thierry allard saint albin <thierry.allardsaintalbin@gmail.com>
2015-05-17 16:34:26 +0200

    Commit : Impossible d'utiliser linq depuis un projet initialisé sous visual studio

--- Sudoku/Sudoku/Cell.cs
+++ Sudoku/Sudoku/Cell.cs
@@ -1,22 +1,64 @@
 ﻿using System;
 using System.Linq;
 using System.Collections;
+using System.Collections.Generic;
 
 
 namespace Sudoku
 {
 	public class Cell
 	{
-		private Ensemble listColumn;
-		private Ensemble listLine;
-		private Ensemble listSector;
+		enum ErrorCell{ExistsInLine,ExistsInColumn,ExistsInSector,notExistsInValues};
+		private Ensemble listColumn {
+			get {
+				return this->listColumn;
+			}
+			set{
+				this->listColumn = value;
+			}
+		}
+		private Ensemble listLine{
+			get {
+					return this->listLine;
+			}
+			set{
+					this->listLine = value;
+			}
+		}
+		private Ensemble listSector {
+			get {
+				return this->listLine;
+			}
+			set {
+				this->listLine = value;
+			}
+		}
 
-		private ArrayList<String> hypothesis;
+		private List<String> hypothesis;
 		private String values;
 
-		public Cell ()
+		public Cell (Ensemble listColumn , Ensemble listLine , Ensemble listSector,String values,List<String> hypothesis)
 		{
+			this->listColumn = listColumn;
+			this->listLine = listLine;
+			this->listSector = listSector;
+			this->values = values;
+			this->hypothesis = hypothesis;
 		}
+
+		public Cell (String values,List<String> hypothesis)
+		{
+			Cell (null, null, null, values, hypothesis);
+		}
+
+
+		public bool ExistsInEnsemble( Ensemble t)
+		{
+			return true;
+				
+		}
+
+
 	}
 }
 
--- Sudoku/Sudoku/Ensemble.cs
+++ Sudoku/Sudoku/Ensemble.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿
+using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
 
 namespace Sudoku
 {
 	public class Ensemble
 	{
-		public Ensemble ()
+		protected internal List<Cell> cellsList
+		{ get; set; }
+		public Ensemble (List<Cell> cellsList)
 		{
+			this->cellsList = cellsList;
+		
 		}
 	}
 }
