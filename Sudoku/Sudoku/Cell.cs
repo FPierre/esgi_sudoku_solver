ced5ffbe1d35d07f972771d9b0e37e9d9181a84e
thierry allard saint albin <thierry.allardsaintalbin@gmail.com>
2015-05-17 15:02:23 +0200

    Modification de Cells

--- Sudoku/Sudoku/Cell.cs
+++ Sudoku/Sudoku/Cell.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Linq;
+using System.Collections;
+
 
 namespace Sudoku
 {
 	public class Cell
 	{
+		private Ensemble listColumn;
+		private Ensemble listLine;
+		private Ensemble listSector;
+
+		private ArrayList<String> hypothesis;
+		private String values;
+
 		public Cell ()
 		{
 		}
